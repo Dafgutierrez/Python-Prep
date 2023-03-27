694f8aaaad720159025b8f13e2e7dec407d31f10
Dafne gutierrez <dafne.m.gutierrez.h@gmail.com>
2023-03-27 12:14:24 -0600

    first commit

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,12 +1,12 @@
 mi_variable = 12
 print(mi_variable)
 
-mi_variable2 = 'dario'
+mi_variable2 = 'dafnee'
 print(mi_variable2)
 
-mi_complejo = 5 + 7j
+mi_complejo = 6 + 6j
 print(mi_complejo)
 
 
-mi_numero = input('Ingrese un valor')
-print('el valor ingresado fue', mi_numero)
+mi_numero = input('12')
+print('12', mi_numero)
